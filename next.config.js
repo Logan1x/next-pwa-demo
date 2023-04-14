19b8d3e66a2bc72bf068a4cf615c40c243c7e468
logan1x <sharmakhushal78@gmail.com>
2023-04-14 15:52:19 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
